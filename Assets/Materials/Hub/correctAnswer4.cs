59fe18aa87af8e0307984c4cb545b3f54c83a828
Miguel Guerra-Mondragon <chipymiguel@yahoo.com>
2021-03-10 00:35:13 -0700

    Upload Project

---
+++ Assets/Materials/Hub/correctAnswer4.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class correctAnswer4 : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPrefs.SetInt("correctQuiz5", 0);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.CompareTag ("Suite105"))
+		{
+			PlayerPrefs.SetInt("correctQuiz5", 1);
+		}
+        else if (other.CompareTag("Suite101") || other.CompareTag("Suite102") ||
+            other.CompareTag("Suite103") || other.CompareTag("Suite104"))
+        {
+            PlayerPrefs.SetInt("correctQuiz5", 2);
+        }
+
+        int count = PlayerPrefs.GetInt("AnswersMoved", 0);
+        count++;
+        PlayerPrefs.SetInt("AnswersMoved", count);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Suite101") || other.CompareTag("Suite102") ||
+            other.CompareTag("Suite103") || other.CompareTag("Suite104") ||
+            other.CompareTag("Suite105"))
+        {
+            PlayerPrefs.SetInt("correctQuiz5", 0);
+        }
+
+        int count = PlayerPrefs.GetInt("AnswersMoved", 0);
+        count--;
+        PlayerPrefs.SetInt("AnswersMoved", count);
+    }
+}
